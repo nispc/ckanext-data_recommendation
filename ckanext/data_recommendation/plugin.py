abb35736f1455a6c8356c1d4d1027014d8c52f53
nispc <nispc@nispc.tw>
2015-09-20 14:37:59 +0800

    adjust

--- ckanext/data_recommendation/plugin.py
+++ ckanext/data_recommendation/plugin.py
@@ -1,9 +1,10 @@
 import ckan.plugins as plugins
 import ckan.plugins.toolkit as toolkit
+from ckan.plugins.toolkit import asbool
 import jieba
 import jieba.analyse
 from ckan.plugins.toolkit import request, c
-import os
+import pylons.config as config
 
 class Data_RecommendationPlugin(plugins.SingletonPlugin):
     plugins.implements(plugins.IConfigurer)
@@ -16,43 +17,38 @@ class Data_RecommendationPlugin(plugins.SingletonPlugin):
         toolkit.add_public_directory(config_, 'public')
         toolkit.add_resource('fanstatic', 'data_recommendation')
 
-    '''@classmethod
-    def pkg_title(cls):
-        pkg_name = request.environ['PATH_INFO'].split('/')[-1]
-        pkg_title = toolkit.get_action('package_show')({}, {'id':pkg_name})['title']
-        jieba.set_dictionary('dict.txt.big')
-        pkg_title = jieba.analyse.extract_tags(pkg_title, topK=5)
-        return pkg_title'''
-
     @classmethod
     def related_pkgs(cls):
-        extract_tags_num = 5
+        # Parameter
+        extractNum = int(config.get('ckan.data_recommended.extract_num', '5'))
+        byTag = asbool(config.get('ckan.data_recommended.by_tag', 'true'))
+        byTitle = asbool(config.get('ckan.data_recommended.by_title', 'true'))
 
+        # fetch pkg info
         pkg_name = request.environ['PATH_INFO'].split('/')[-1]
-
         pkg_title = toolkit.get_action('package_show')({}, {'id':pkg_name})['title']
+        pkg_tags = [pkg_tag['name'] for pkg_tag in toolkit.get_action('package_show')({}, {'id':pkg_name})['tags']]
 
-        pkg_tags_result = toolkit.get_action('package_show')({}, {'id':pkg_name})['tags']
-        pkg_tags = [pkg_tag['name'] for pkg_tag in pkg_tags_result]
+        related_tag_titles = {}
 
-        related_tag_titles = []
+        if byTag:
+            related_tag_titles.update(set(pkg_tags))
 
-        if True:
-            related_tag_titles += pkg_tags
-
-        if True:
+        if byTitle:
             jieba.set_dictionary('dict.txt.big')
-            related_tag_titles += jieba.analyse.extract_tags(pkg_title, topK=extract_tags_num)
+            related_tag_titles.update(
+                set(
+                    jieba.analyse.extract_tags(pkg_title, topK=extractNum)
+                )
+            )
 
-        related_pkgs = {}
+        related_pkgs = dict()
         for related_tag_title in related_tag_titles:
-            related_pkg_results = toolkit.get_action('package_search')({}, {'q': related_tag_title})['results']
+            related_pkg_results = toolkit.get_action('package_search')({}, {'q': related_tag_title, 'rows': 3})['results']
 
             related_pkgs[related_tag_title] = related_pkg_results
 
         return related_pkgs
-
-
 
     def get_helpers(self):
         return {'related_pkgs': self.related_pkgs}
